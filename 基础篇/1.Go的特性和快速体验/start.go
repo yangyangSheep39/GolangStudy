dc26495172b723ae4e7d52352e2029ce54afd4ec
yangyangSheep <fangyang98@163.com>
2021-03-19 23:36:24 +0800

    first push

---
+++ 基础篇/1.Go的特性和快速体验/start.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+/*
+ * @Description: Go的特性快速体验
+ * @Autor: yangyangSheep
+ * @Date: 2021-03-19 19:20:29
+ * @LastEditors: yangyangSheep
+ * @LastEditTime: 2021-03-19 19:22:22
+ */
+ func sayHello(){
+	 //输出一句话
+	 fmt.Println("Hello World")
+ }
+
